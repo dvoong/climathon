e34563d2c9545b624216b24bf8d042e846aded38
David Voong <voong.david@gmail.com>
2015-06-19 05:50:15 -0400

    Added property specific no2 indices

--- climathon/views.py
+++ climathon/views.py
@@ -21,7 +21,7 @@ def search(request, lng, lat):
         postcode_info = urllib2.urlopen('http://www.uk-postcodes.com/postcode/{}.json'.format(postcode)).read()
         postcode_info = json.loads(postcode_info)
     properties = [{'latitude': x.split(',')[0], 'longitude': x.split(',')[1]} for x in request.GET.getlist('property')]
-    print 'properties: {}'.format(properties)
+    # print 'properties: {}'.format(properties)
     max_dist = 5.
     if 'max-dist' in request.GET:
         max_dist = float(request.GET['max-dist'])
@@ -82,6 +82,7 @@ def search(request, lng, lat):
                 avg_no2[date] += int(no2_index)
                 counter[date] += 1
         output["daily_no2_index"] = list(reversed(output["daily_no2_index"]))
+        site['daily_no2_index'] = output['daily_no2_index']
         output2['sites'] += [output]
     # averaged data
     avg_data = {'dist': 0.}
@@ -101,10 +102,28 @@ def search(request, lng, lat):
     for p in properties:
         lat = p['latitude']
         lng = p['longitude']
-        avg_no2_index = 0
-        # total_distance = 0
-        # n_sites_queried = 0
-        # for site in ordered_sites:
+        property_avg_no2_index = 0.
+        total_distance = 0.
+        n_sites_queried = 0
+        for site in ordered_sites:
+            if site['dist'] > 1. * max_dist:
+                continue
+            if len(site['daily_no2_index']) != 0:
+                total_distance += site['dist']
+                n_sites_queried += 1
+                # print site['@SiteName'], site['dist']
+        # print 'total_distance: {}'.format(total_distance)
+        for site in ordered_sites:
+            if site['dist'] > 1. * max_dist:
+                continue
+            avg_no2_index = 0.
+            sum = 0
+            for x in site['daily_no2_index']:
+                sum += x['no2_index']
+            if sum:
+                avg_no2_index = sum * 1. / len(site['daily_no2_index'])
+            property_avg_no2_index += avg_no2_index * site['dist'] * 1. / total_distance
+        #     counter = 0
         #     for i, daily_air_quality in enumerate(valid_air_quality):
         #         date = (current_date - datetime.timedelta(days=1) - datetime.timedelta(days=i)).date()
         #         daily_air_quality = json.loads(daily_air_quality.content)
@@ -118,27 +137,12 @@ def search(request, lng, lat):
         #             if species["@SpeciesCode"] == "NO2":
         #                 no2_index = species["@AirQualityIndex"]
         #         if no2_index != None:
-        #             total_distance += site['dist']
-        #             n_sites_queried += 1
-        # for site in ordered_sites:
-        #     for i, daily_air_quality in enumerate(valid_air_quality):
-        #         date = (current_date - datetime.timedelta(days=1) - datetime.timedelta(days=i)).date()
-        #         daily_air_quality = json.loads(daily_air_quality.content)
-        #         no2_index = None
-        #         species = daily_air_quality["DailyAirQualityIndex"]["LocalAuthority"]["Site"]["Species"]
-        #         if type(species) == list:
-        #             for x in species:
-        #                 if x["@SpeciesCode"] == "NO2":
-        #                     no2_index = int(x["@AirQualityIndex"])
-        #         elif type(species) == dict:
-        #             if species["@SpeciesCode"] == "NO2":
-        #                 no2_index = species["@AirQualityIndex"]
-        #         if no2_index != None:
-        #             avg_no2_index += no2_index * site['dist'] * 1. / total_distance
-        
-        # if n_sites_queried >= 1:
-        #     avg_no2_index = 1. * avg_no2_index / n_sites_queried
-        p['avg_no2_index'] = avg_no2_index
+        #             avg_no2_index += no2_index
+        #             counter += 1
+        #     if counter >= 0:
+        #         avg_no2_index = 1. * avg_no2_index / counter
+        #         property_avg_no2_index = property_avg_no2_index + avg_no2_index * site['dist'] * 1. / total_distance
+        p['avg_no2_index'] = property_avg_no2_index
     output2['properties'] = properties
             
     return HttpResponse(json.dumps(output2), content_type="application/json")
